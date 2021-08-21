c41e684a78ac1ded184fc54af77873c3de2b7e4c
Llirakor <87139047+Llirakor@users.noreply.github.com>
2021-08-21 14:35:43 -0300

    Add files via upload

---
+++ TomasRodriguezEjercicio1/TomasRodriguezEjercicio1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TomasRodriguezEjercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Cateto1, Cateto2;
+
+            Console.Write("Ingrese El Lado 1: ");
+            Cateto1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese El Lado 2: ");
+            Cateto2 = int.Parse(Console.ReadLine());
+
+            double Hipotenusa = Math.Sqrt(Math.Pow(Cateto1, 2) + Math.Pow(Cateto2, 2));
+            Console.WriteLine("La hipotenusa es: " + Hipotenusa);
+
+            double Superficie = (Cateto1 + Cateto2) / 2;
+            Console.WriteLine("El perimetro es: " + Superficie);
+
+            double Perimetro = Cateto1 + Cateto2 + Hipotenusa;
+            Console.WriteLine("El perimetro es: " + Perimetro);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TomasRodriguezEjercicio3/TomasRodriguezEjercicio3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace TomasRodriguezEjercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Celsius, Fahrenheit, MayorTemperatura;
+            double Promedio = 0;
+
+            int CantidadX;
+
+            do
+            {
+                Console.Write("Ingrese la cantidad de temperaturas a evaluar: ");
+                if (!int.TryParse(Console.ReadLine(), out CantidadX))
+                {
+                    Console.WriteLine("Cantidad mal ingresada");
+
+                }
+                else if (CantidadX <= 0 || CantidadX > 20)
+                {
+                    Console.WriteLine("La cantidad debe ser positiva e inferior o igual a 20");
+                }
+                else
+                {
+                    break;
+                }
+            } while (true);
+            MayorTemperatura = -71;
+            for (int contador = 0; contador < CantidadX; contador++)
+            {
+                do
+                {
+                    Console.Write($"Ingrese la temperatura {contador + 1}:");
+                    if (!double.TryParse(Console.ReadLine(), out Celsius))
+                    {
+                        Console.WriteLine("Cantidad mal ingresada");
+                    }
+                    else if (Celsius < -70 || Celsius > 60)
+                    {
+                        Console.WriteLine("La temp debe estar en el rango -70 y 60 grados");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                } while (true);
+                Fahrenheit = ConvertirFahrenheit(Celsius);
+                Console.WriteLine($"{Celsius} equivalen a {Fahrenheit} grados F");
+                if (Celsius > MayorTemperatura)
+                {
+                    MayorTemperatura = Celsius;
+                }
+                Promedio += Celsius;
+            }
+            Promedio /= CantidadX;
+            Console.WriteLine($"La mayor temperatura es {MayorTemperatura}");
+            Console.WriteLine($"La media de temperatura es {Promedio}");
+            Console.ReadLine();
+        }
+        private static double ConvertirFahrenheit(double grados)
+        {
+            return 1.8 * grados + 32;
+        }
+    }
+}
